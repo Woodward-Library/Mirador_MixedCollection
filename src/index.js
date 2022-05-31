30da73714f6f53a058b17ffe4fbc0fecb69fb74b
wdrefubc <73660886+wdrefubc@users.noreply.github.com>
2022-05-31 14:02:22 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ Mirador.viewer({
 
   windows: [
     {
-      "loadedManifest": "https://raw.githubusercontent.com/Woodward-Library/Mirador_MixedCollection/main/src/mixed_manifest.json",
+      "loadedManifest": "https://raw.githubusercontent.com/Woodward-Library/Mirador_MixedCollection/main/src/mixed_manifest5.json",
       "canvasIndex": 0,
       sideBarOpen: true,
       view:'gallery',
